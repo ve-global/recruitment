5f5edebac66e2ee60bd11d9c8c0836d7aea1d192
sohale <sohale@gmail.com>
2016-09-26 01:27:23 +0100

    Variable name in test changed: hasBeenStored -> valueChanged. Config cleanup. Slight change of some versions. tests defined in 'window' and 'classes'

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,40 @@
 // karma.conf.js
 module.exports = function(config) {
     config.set({
-        frameworks: ['jasmine'],
+        frameworks: [
+            'jasmine'
+        ],
         files: [
             'JS/Classes/DataReporter.js',
-            'Test/**'
+            'Test/**',
+            'Test/Classes/DataReporterTest.js',
+            'spec/sohail/my1Spec.js',
         ],
+
+        plugins : [
+          'karma-chrome-launcher',
+          //'karma-firefox-launcher',
+          // 'karma-phantomjs-launcher',
+          'karma-jasmine',
+          //'karma-jasmine-html-reporter'
+        ],
+
+        browsers: [ //'PhantomJS',
+            'Chrome'],
+
+
+            // new:
+        reporters: [
+            // 'html',  //cannot load 'html' it is not regitered.
+            'progress',
+            //'junit',
+            //'coverage',
+             ],
+
+        coverageReporter: {
+            type: 'html',
+            dir: 'coverage/'
+        },
+
     });
 };
--- spec/sohail/my1Spec.js
+++ spec/sohail/my1Spec.js
@@ -1,2 +1,13 @@
+(function(win, classes){
+  'use strict';
+
 describe("MyFirstTest", function() {
+    it("test if Jasmine works", function(){
+        console.log("Jasmine works.");
+        expect(true).toBe(true);
+    })
+    console.log("Setting up the test.");
+    // classes.
 });
+
+}(window, classes));

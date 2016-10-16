e20268dd30627029097158c15229ec8d7e4a7e81
sohale <sohale@gmail.com>
2016-10-16 19:37:25 +0100

    more cleanup

--- JS/Classes/Visualiser.js
+++ JS/Classes/Visualiser.js
@@ -3,31 +3,13 @@ var KLEPTO = KLEPTO || {};
 (function(win, KLEPTO){
     'use strict';
 
-    function Visualiser(document) {
-        this.setup(document, win);
+    function Visualiser(document_) {
+        this.setup(document_, win);
+        this.document_ = document_;
     }
 
-    //Visualiser.prototype.register_into_dom = function (document_) {
-    Visualiser.prototype.setup = function (document_, window) {
+    Visualiser.prototype.setup = function (document_, window_) {
 
-        /*
-        // http://www.w3schools.com/howto/howto_css_modals.asp
-        var html0 = `<!-- The Modal -->
-        <div id="myMiniPanel" class="modal">
-
-          <!-- Modal content -->
-          <div class="modal-content">
-            <span class="close">x</span>
-            <div id=visualiser_content_main>
-            <p >Some text in the Modal..</p>
-            </div>
-          </div>
-        </div>`;
-        */
-
-        // relative: http://www.w3schools.com/css/tryit.asp?filename=trycss_position_absolute
-        // <div id=visualiser_content_main style="position: absolute; top: 10px; right: 30px; width: 200px; height: 100px; border: 3px solid #73AD21;">
-        // <div id=visualiser_content_main style="position: fixed; top: 10px; right: 10px; width: 200px; height: 100px; border: 3px solid #73AD21;">
 
         var html = `<!-- The Modal -->
         <div>
@@ -39,26 +21,15 @@ var KLEPTO = KLEPTO || {};
             </div>
         </div>`;
 
-        //console.log("************************");
-        //console.log(html);
-
         var dom = document_.querySelector("#visualiser_content_main");
         if (dom) {
             console.error("Already added.");
             return;
         }
-        // var node = document_.getElementById("myList2").lastChild;
-        //document_.getElementById("body").appendChild(node);;
 
         var temp = document_.createElement('div');
         temp.innerHTML = html;
-        // var htmlObject = temp.firstChild;
         var body = document.querySelector("body"); //document_.getElementById("body");
-        // console.log(body);
-        /*
-        var lastnode = body.lastChild;
-        lastnode.appendChild(temp);
-        */
         body.appendChild(temp);
 
 
@@ -69,17 +40,8 @@ var KLEPTO = KLEPTO || {};
         // appear
         this.appear();
 
+        // Make a reference to this in the closure
         var that = this;
-
-        /*
-        // Get the button that opens the modal
-        //var btn = document.getElementById("myBtn");
-        // When the user clicks on the button, open the modal
-        btn.onclick = function() {
-            this.appear();
-        }
-        */
-
 
         // Get the <span> element that closes the modal
         var span = document.getElementsByClassName("close")[0];
@@ -88,17 +50,26 @@ var KLEPTO = KLEPTO || {};
             that.disappear();
         }
 
+
         /*
+        // The button that opens the modal
+        //var btn = document.getElementById("myBtn");
+        // When the user clicks on the button, open the modal
+        btn.onclick = function() {
+            that.appear();
+        }
+        */
+
+
+        /*
+        // Modal only:
         // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
+        window_.onclick = function(event) {
             if (event.target == modal) {
                 that.disappear();
             }
         }
         */
-
-        this.document_ = document_;
-
     }
     Visualiser.prototype.appear = function () {
         // this.modal.style.display = "block";
